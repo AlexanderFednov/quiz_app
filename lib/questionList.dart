b86c0e374cd7dd39ccb00e6576c1550e9920cde5
AlexanderFednov <78268652+AlexanderFednov@users.noreply.github.com>
2021-03-09 19:08:15 +0300

    Create questionList.dart

---
+++ lib/questionList.dart
@@ -0,0 +1,12 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:flutter/material.dart';
+
+@JsonSerializable()
+class QuestionList {
+  List<QuestionInside> question;
+}
+
+class QuestionInside {
+  String questionText;
+  List<Map<String, Object>> answers;
+}
