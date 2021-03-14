42e66312282bac50ec6a3ca29727bb4778e21981
AlexanderFednov <78268652+AlexanderFednov@users.noreply.github.com>
2021-03-14 20:46:15 +0300

    Update questionList.dart

--- lib/questionList.dart
+++ lib/questionList.dart
@@ -4,17 +4,19 @@ import 'package:json_annotation/json_annotation.dart';
 @JsonSerializable()
 class QuestionList {
   List<QuestionInside> question;
+  QuestionList(this.question);
 }
 
 class QuestionInside {
   String questionText;
   List<Answers> answers;
+  QuestionInside(this.questionText, this.answers);
 }
 
 class Answers {
-  String questionText;
+  String text;
   bool result;
   String code;
 
-  Answers(this.questionText, this.result, this.code);
+  Answers(this.text, this.result, this.code);
 }
