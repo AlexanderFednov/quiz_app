834d171c459fba3fb854c3e9e4deb3f72a213fb4
AlexanderFednov <78268652+AlexanderFednov@users.noreply.github.com>
2021-10-18 17:51:05 +0300

    Update localization_bloc.dart

--- lib/localization/localization_bloc.dart
+++ lib/localization/localization_bloc.dart
@@ -3,9 +3,12 @@ import 'dart:ui';
 import 'package:quiz_app/localization/localization_model.dart';
 import 'package:rxdart/subjects.dart';
 import 'package:easy_dispose/easy_dispose.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LocalizationBloc extends DisposableOwner {
   LocalizationBloc() {
+    _loadLocale();
+
     _localizationStateSubject.disposeWith(this);
   }
 
@@ -21,19 +24,54 @@ class LocalizationBloc extends DisposableOwner {
 
   Locale get currentLocale => _localizationStateSubject.value.currentLocale;
 
-  void localeRu() {
+  void localeRu() async {
     _localizationStateSubject.add(
       localizationState.copyWith(
         currentLocale: Locale('ru', 'RU'),
       ),
     );
+
+    var prefs = await SharedPreferences.getInstance();
+
+    await prefs.setInt('localeIndex', 0);
   }
 
-  void localeEn() {
+  void localeEn() async {
     _localizationStateSubject.add(
       localizationState.copyWith(
         currentLocale: Locale('en', 'EN'),
       ),
     );
+
+    var prefs = await SharedPreferences.getInstance();
+
+    await prefs.setInt('localeIndex', 1);
+  }
+
+  void _loadLocale() async {
+    var prefs = await SharedPreferences.getInstance();
+
+    final localeIndex = prefs.getInt('localeIndex');
+
+    switch (localeIndex) {
+      case 0:
+        _localizationStateSubject.add(
+          localizationState.copyWith(
+            currentLocale: Locale('ru', 'RU'),
+          ),
+        );
+        break;
+
+      case 1:
+        _localizationStateSubject.add(
+          localizationState.copyWith(
+            currentLocale: Locale('en', 'EN'),
+          ),
+        );
+        break;
+
+      default:
+        null;
+    }
   }
 }
